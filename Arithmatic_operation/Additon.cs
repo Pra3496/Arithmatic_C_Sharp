f063ffba88f204190f7cb97ed72339332e4aadee
Pranav Waghmare <piyushw55@gmail.com>
2022-12-10 12:15:05 +0530

    Addition operation

---
+++ Arithmatic_operation/Additon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmatic_operation
+{
+    internal class Additon
+    {
+        public int Addition(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
--- Arithmatic_operation/Program.cs
+++ Arithmatic_operation/Program.cs
@@ -7,8 +7,8 @@
             int first = 0;
             int second = 0;
 
-            Console.WriteLine("Calculator Class -> Addition");
-            Calculator cal = new Calculator();
+            Console.WriteLine("Addition Class -> Addition");
+            Additon cal = new Additon();
             
 
             Console.WriteLine("Enter the First Number :");
@@ -18,9 +18,6 @@
             second = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Sum of {0} and {1} is {2}", first, second, cal.Addition(first, second));
-            Console.WriteLine("Diffrence of {0} and {1} is {2}", first, second, cal.Substraction(first, second));
-            Console.WriteLine("Multiplication of {0} and {1} is {2}", first, second, cal.Multiplication(first, second));
-            Console.WriteLine("Division of {0} and {1} is {2}", first, second, cal.division(first, second));
 
         }
     }
